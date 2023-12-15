443c72baef56889e1111235b4b234ccb34ef1a9c
KASSTUSS <kostya2003112015@gmail.com>
2023-12-15 21:13:04 +0300

    feat: add colors constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,24 @@
+export const enum COLORS {
+  MAIN = "#268362",
+  MAIN_RED = "#922626",
+  UNSELECTED = "#8B9F98",
+  MAIN_TEXT = "#40534C",
+
+  UNSELECTED_ACCOUNT = "#B3B3B3",
+  BACKGROUND_BLOCK = "#E8E8E8",
+
+  CHART_GRADIRNT_FROM = "#257E5F",
+  CHART_GRADIRNT_TO = "#32AB81",
+}
+
+export const colorsGradients = [
+  ["#d87498", "#e1d3b5"],
+  ["#d4ccd3", "#d08d72"],
+  ["#a4bf9f", "#7fb28e", "#7c9eb6"],
+  ["#a4bf9f", "#e68e96"],
+  ["#9280c8", "#b7e9da"],
+  ["#5081ae", "#8ccfc2", "#d3b2c9"],
+  ["#b53e67", "#d17a99", "#b06a88"],
+  ["#cc4a77", "#d987a9"],
+  ["#ba02c0", "#420fcb"],
+];
